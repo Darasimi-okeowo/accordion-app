5d0631f17b423fa7c2c892d126e88cf105e93075
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-08-03 16:40:20 +0100

    second-commit

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -4,25 +4,25 @@ import {AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
 
 const Accordion = () => {
 
-    const [questions,setQuestions] = useState(Data)
+    const [questions,setQuestions] = useState(Data)  
 
     const handleClick = (index) => {
         const lists = [...questions];
         const curQues = lists[index];
-        console.log(curQues)
+        // console.log(curQues)
         curQues.checked = !curQues.checked;
         setQuestions(() => lists)
     }
     return (
-    <div>
+    <div className='main'>
       {questions.map((item, index) => {
         return (
           <>
-            <div  key={index}>
+            <div  key={index} className='questions'>
               <h3>{item.questions}</h3>
               <span onClick={() => handleClick(index)}>{item.checked  ? <AiOutlinePlus /> : <AiOutlineMinus />}</span>
             </div>
-            {item.checked && <p>{item.answer}</p>}
+            {item.checked && <p className='items'>{item.answer}</p>}
           </>
         );
       })}
